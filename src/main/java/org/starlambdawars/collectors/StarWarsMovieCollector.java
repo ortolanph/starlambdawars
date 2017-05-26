c9591d0147c9401b453d2d9aad4df8f914348e9b
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2017-05-26 00:58:51 -0300

    Collectors

--- src/main/java/org/starlambdawars/collectors/StarWarsMovieCollector.java
+++ src/main/java/org/starlambdawars/collectors/StarWarsMovieCollector.java
@@ -1,15 +1,19 @@
 package org.starlambdawars.collectors;
 
 import org.starlambdawars.beans.ForceAlignment;
+import org.starlambdawars.beans.MovieType;
 import org.starlambdawars.beans.StarWarsCharacter;
 import org.starlambdawars.beans.StarWarsMovie;
 import org.starlambdawars.mapper.StarWarsMovieMapper;
 import org.starlambdawars.utils.DataLoader;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StarWarsMovieCollector {
 
@@ -24,24 +28,41 @@ public class StarWarsMovieCollector {
 //
 //        return characters
 //                .stream()
+//                .map(a -> a.getForceAlignment())
 //                .collect(
 //                        Collectors.toMap(
-//                                a -> a.getForceAlignment(),
-//                                characters.stream().filter(c -> c.getForceAlignment().equals(a)).collect(Collectors.toList())
+//                                ,
+//                                findByForceAlignment(a, characters)
 //                        )
 //                );
 //    }
 //
-//    public Map<MovieType, List<String>> mapTypeByMovies() {
-//        return Arrays
-//                .stream(MovieType.values())
+//    public Map<MovieType, List<StarWarsMovie>> mapTypeByMovies() {
+//        return movies
+//                .stream()
+//                .map(k -> k.getKind())
 //                .collect(
 //                        Collectors.toMap(
-//                                ,
-//                                movies.stream().filter(m -> m.getKind().equals(t)).collect(Collectors.toList())
+//                                Function.identity(),
+//                                findByKind(k, movies)
 //                        )
 //                );
 //    }
+
+    private List<StarWarsCharacter> findByForceAlignment(ForceAlignment alignment, List<StarWarsCharacter> characters) {
+        return characters
+                .stream()
+                .filter(c -> c.getForceAlignment().equals(alignment))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> findByKind(MovieType kind, List<StarWarsMovie> movies) {
+        return movies
+                .stream()
+                .filter(m -> m.getKind().equals(kind))
+                .map(m -> m.getTitle())
+                .collect(Collectors.toList());
+    }
 
     private DataLoader loader;
     private List<StarWarsMovie> movies;
